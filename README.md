cf951484daf5e1c8f3e7f576b116432b497ed1d4
fuzzier <630533571@qq.com>
2018-07-11 17:53:54 +0800

    prune n-node tree

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # Nginx Config Transfer
 
+### basic structure
+1. n-node tree
+2. SpecialNode: base n-node tree
+
 ### nginx.confg ---> n-node tree structure
 
-1. nginx.confg --> in_list `[list[list]]`
+1. nginx.confg ---> in_list `[list[list]]`
 2. in_list ---> n-node tree
+3. n-node tree ---> n-node tree after prune
+
+### access way
+`bn_obj.http.server.error_page` res: `LIST [404, 500 502 503 504]`
+`bn_obj.http.server.error_page[1]` res: `STRING /50x.html;`
+`bn_obj.http.server.location[2]` res: `DICT: ['index', 'root']`
+`bn_obj.http.server.location[2].index` res: `STRING 404.html;`
--- parser_config.py
+++ parser_config.py
@@ -2,23 +2,15 @@
 # coding=utf-8
 
 import re
+import time
 
 
-# tree structure
-class Node(object):
-
-    def __init__(self, val, children):
-        self.val = val
-        self.children = children
-
-    def __repr__(self):
-        return '{val} --> {children}'.format(val=self.val, children=self.children)
-
-
-# solve file change to in_list
+# solve file change to in_list and string problem
 class Solution(object):
 
-    # change list to in_list by backtracking(core_method)
+    item_set = {'error_page'}
+
+    # change list to in_list by backtracking(core method)
     def backTracking(self, lst, i_dict, temp):
         '''
         :param lst: list
@@ -55,57 +47,210 @@ class Solution(object):
         return temp_list
 
     # combine read_file and backtracking methods
-    def toStructure(self, filename):
+    def toInList(self, filename):
         lst = self.read_file(filename)
         in_list = self.backTracking(lst, {'i': 0}, [])
         return in_list
 
+    # self-bulid a match space method  分割两个空号的方法
+    # def match_space(self, s):
+    #     pattern = '  '
+    #     res_list = []
+    #     i = 0
+    #     while i < len(s)-1:
+    #         if s[i] == pattern[0] and s[i+1] == pattern[1]:
+    #             res_list.append(s[:i].strip())
+    #             res_list.append(s[i+1:].strip())
+    #             break
+    #         else:
+    #             i += 1
+    #     else:  # have not value
+    #         res_list.append(s)
+    #         res_list.append(' ')
+    #     return res_list[0], res_list[1]
+
+    # self-bulid a match space method, 可扩展
+    def match_space(self, s):
+        key_temp = s.split()
+        key_first = key_temp[0]
+        if key_first in self.item_set:
+            key = ' '.join(key_temp[:-1])
+            value = key_temp[-1]
+            return [key, value]
+        else:
+            return key_temp
+
+
+# n-node-tree + container structure
+class Node(object):
+
+    def __init__(self, val, children=None):
+        self.val = val  # string
+        self.children = children  # Node or dict
+
+    # user interface
+    def __str__(self):
+        ele = self.children
+        if isinstance(self.children, dict):
+            return 'DICT: {ele}'.format(ele=str(self.children.keys()))
+        elif isinstance(self.children, list):
+            return 'LIST {ele}'.format(ele=str(self.children))
+        else:  # string
+            return 'STRING {ele}'.format(ele=ele.val)
+
+    # debug
+    def __repr__(self):
+        return '{val}'.format(val=self.val)
+        # return '{val} --> {children}'.format(val=self.val, children=self.children)
+
+    def __getattr__(self, item):
+        child = self.children[item]
+        return child
+
+    def __getitem__(self, item):
+        # if item in self.item_set:
+        #     child_list = self.collectChild(item)  # list
+        #     return str(child_list)
+        # else:
+
+        # if isinstance(self.children, list)
+        child = self.children[item]
+        return child
+
+    # def collectChild(self, item):
+    #     '''
+    #     :param item: string
+    #     :return: list
+    #     '''
+    #     child_list = [ele for ele in self.children.keys() if ele.split(' ')[0] == item[:-1]]
+    #     return child_list
+
+
+class SpecialNode(Node):
+    # Include: location, error_page, ...
+
+    def __init__(self, special_val, children):
+        '''
+        :param special_val: string
+        :param children: list
+        '''
+        super(SpecialNode, self).__init__(special_val, children)
+
 
 class BulidNode(object):
 
+    def __init__(self):
+        self.solve = Solution()
+        self.item_set = {'location', 'upstream', 'error_page'}  # special item
+
     # build n-node tree(core method)
-    def build(self, lst):
+    def build(self, in_list):
         '''
-        :param lst:  嵌套list
+        :param in_list:  嵌套list
         :return: root(n-node)
         '''
         root = Node('nginx', {})  # set root
+
         def dfs(root, lst):  # dfs travel
             for ele in lst:
                 if isinstance(ele, list):
-                    index_line = ele[0].find('{')
-                    key = ele[0][:index_line].strip()
+                    index_line = ele[0].find('{')  # ele[0]: "location /publishManage  {"
+                    key = ele[0][:index_line].strip()  # key: "location /publishManage"
                     n = Node(key, {})
                     dfs(n, ele[1:-1])  # recursive dfs
                     # print 'debug'
-
                 else:
-                    line_temp = ele.split()
-                    key, value = line_temp[0], line_temp[1]
+                    ele_temp = self.solve.match_space(ele)
+                    key, value = ele_temp[0], Node(' '.join(ele_temp[1:]))  # ['error_page', '500 /50x.html']
                     n = Node(key, value)
-
                 root.children[n.val] = n
-        dfs(root, lst)
+
+        dfs(root, in_list)
+        return root
+
+    # take pruning to tree(core method)
+    def prune(self, root):
+        '''
+        :param root: n-node-tree structure
+        :return: None
+        '''
+        # init {item: SpecialNode,...} 可优化 item_dict.clear()
+        item_dict = {}
+        for item in self.item_set:
+            item_dict[item] = SpecialNode(item, [])
+
+        def bfs(root):  # bfs travel
+            queue = [root]
+            while queue:
+                for _ in xrange(len(queue)):
+                    root = queue.pop(0)
+                    children = root.children  # Node or dict
+                    # key_temp = root.val.split()  # ["location",  "/publishManage"]
+                    # key_first = key_temp[0]  # "location"
+                    if isinstance(children, dict):
+                        queue.extend(children.values())  # queue add
+
+                        for child_name in children.keys():  # prune part
+                            key_temp = children[child_name].val.split()  # ["location",  "/publishManage"]
+                            key_first = key_temp[0]  # "location"
+                            if key_first in item_dict:
+                                if not children.get(key_first):  # 如果还没有SpecialNode，需要新建
+                                    # print key_first
+                                    children[key_first] = item_dict[key_first]  # new build SpecialNode(key_first, [])
+                                child = children.pop(child_name)  # prune
+                                key_other = ' '.join(key_temp[1:])  # "/publishManage"
+                                child.val = key_other  # update child.val
+                                # print child.val, child.children
+                                children[key_first].children.append(child)  # SpecialNode.children.append(node)
+                    else:  # children(Node)
+                        pass
+
+        def prune_hepler():
+            pass
+
+        bfs(root)
+        item_dict.clear()  # clear
+        return
+
+    # combine build and pruning
+    def buildTree(self, in_list):
+        root = self.build(in_list)
+        self.prune(root)
         return root
 
     def travel(self):
         pass
 
-    def loads(self, filename):
+    def load(self, filename):
         '''
         :return: n-node
         '''
-        in_list = Solution().toStructure(filename)
-        root = self.build(in_list)
+        in_list = self.solve.toInList(filename)
+        root = self.buildTree(in_list)
+        # root = self.build(in_list)  # test build
         return root
 
-    def dumps(self):
+    def dump(self, in_list, filename):
+        '''
+        :param in_list: 嵌套list
+        :return: None
+        '''
         pass
+
 
 if __name__ == '__main__':
     # test
     # lst = Solution().main('nginx.conf')
     # print lst
     # BulidNode().build(lst).children['http'].children['server'].children['location /'].children
-    print BulidNode().loads('nginx.conf').children['http'].children['server'].children['location /'].children
 
+    bn_obj = BulidNode().load('nginx.conf')
+    # print bn_obj['http']['server']['location'].children
+    # print bn_obj['http']['server']['location'][0].children
+    # print bn_obj['http']['server']['error_page'].children
+
+    error_page = bn_obj.http.server.error_page
+    print bn_obj.http.server.error_page[1]
+    print bn_obj.http.server.location
+    print bn_obj.http.server.location[2]
+    print bn_obj.http.server.location[2].index
